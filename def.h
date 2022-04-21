e773c7f8c1beb88a5b2e1177a0d3f16deae5d708
OmarT-Y <omartamerz9@gmail.com>
2022-04-22 00:25:00 +0200

    final edit

--- def.h
+++ def.h
@@ -1,16 +1,16 @@
 struct cashflow
 {
-	int N, FC, VS, AOC;
-	float i;
+    int N, FC, VS, AOC, AR;
+    float i;
 };
 int PV(cashflow f1)
 {
-	float Pval = -f1.FC;
-	Pval += f1.VS / pow(1 + f1.i, f1.N);
-	Pval -= f1.AOC*(1 - (pow(1 + f1.i, f1.N)) / f1.i);
-	return round(Pval);
+    float Pval = -f1.FC;
+    Pval += f1.VS / pow(1 + f1.i, f1.N);
+    Pval += (f1.AR - f1.AOC) * ((1 - (pow(1 + f1.i, -f1.N))) / f1.i);
+    return round(Pval);
 }
-int ACA(int Pval, float i)
+int ACA(cashflow f1)
 {
-
+    return (PV(f1) * f1.i) / (1 - pow(1 + f1.i, -f1.N));
 }
--- main.cpp
+++ main.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 #include <cmath>
+#include "def.h"
+using namespace std;
 int main()
 {
+    cashflow input;
+    cout << "Enter Lifetime: ";
+    cin >> input.N;
+    cout << "Enter First Cost: ";
+    cin >> input.FC;
+    cout << "Enter AOC: ";
+    cin >> input.AOC;
+    cout << "Enter Annual Revenue: ";
+    cin >> input.AR;
+    cout << "Enter Salvage Value: ";
+    cin >> input.VS;
+    cout << "Enter Interest Rate in decimal format: ";
+    cin >> input.i;
 
+    cout << "Present Value = " << PV(input) << endl << "Annual Cost = " << ACA(input);
 }
