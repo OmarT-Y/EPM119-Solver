a09a8c91eb4923bc6ffccb84420f7c76f520aec0
OmarT-Y <omartamerz9@gmail.com>
2022-04-22 01:11:40 +0200

    final edit

--- def.h
+++ def.h
@@ -5,10 +5,10 @@ struct cashflow
 };
 int PV(cashflow f1)
 {
-	float Pval = -f1.FC;
-	Pval += f1.VS / pow(1 + f1.i, f1.N);
-	Pval -= f1.AOC*((1 - (pow(1 + f1.i, f1.N))) / f1.i);
-	return round(Pval);
+    double Pval = -f1.FC;
+    Pval += f1.VS / pow(1 + f1.i, f1.N);
+    Pval += (f1.AR - f1.AOC) * ((1 - (pow(1 + f1.i, -f1.N))) / f1.i);
+    return round(Pval);
 }
 int ACA(cashflow f1)
 {
