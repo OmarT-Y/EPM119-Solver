24eb7be680ec6b9e74adafd95c638ba1409c6371
OmarT-Y <omartamerz9@gmail.com>
2022-04-22 00:25:00 +0200

    edit

--- def.h
+++ def.h
@@ -7,7 +7,7 @@ int PV(cashflow f1)
 {
 	float Pval = -f1.FC;
 	Pval += f1.VS / pow(1 + f1.i, f1.N);
-	Pval -= f1.AOC*(1 - (pow(1 + f1.i, f1.N)) / f1.i);
+	Pval -= f1.AOC*((1 - (pow(1 + f1.i, f1.N))) / f1.i);
 	return round(Pval);
 }
 int ACA(int Pval, float i)
